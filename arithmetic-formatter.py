621c55fd74762d852327432d7fc5d73c54b88478
sethgalyon <github@sethgalyon.slmail.me>
2022-05-21 15:44:16 -0400

    Add arithmetic-formatter.py and function def

---
+++ arithmetic-formatter.py
@@ -0,0 +1,13 @@
+# Arithmetic Formatter
+#
+# input: list of string(s)
+#        (optional) boolean
+#            True  = solve arithmetic problem(s)
+#            False = don't solve arithmetic problem(s)
+#
+# output: arithmetirc problems printed onto terminal line
+#         formatted vertically
+#             (optional) answer to arithmetic problems
+#             added to vertical formatting
+
+def arithmetic_formatter(problems: list = [''], solve: bool = False) -> None:
